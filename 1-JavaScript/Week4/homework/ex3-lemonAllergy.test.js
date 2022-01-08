d364a6db7ade5805a6f944b6793a600cc0c8e931
NOOR HASAN <93947458+noorhazim@users.noreply.github.com>
2022-01-08 15:14:08 +0100

    JS week4 homework

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -16,36 +16,43 @@ Complete the function called `sanitizeFruitBasket`:
 - Return a new array that contains the fruits without any lemons.
 ------------------------------------------------------------------------------*/
 const fruitBasket = [
-  'apple',
-  'lemon',
-  'grapefruit',
-  'lemon',
-  'banana',
-  'watermelon',
-  'lemon',
+    'apple',
+    'lemon',
+    'grapefruit',
+    'lemon',
+    'banana',
+    'watermelon',
+    'lemon',
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(fruitBasket, outFruit) {
+
+    const sanitizeFruit = fruitBasket.filter(fruit => {
+        return fruit !== outFruit;
+    })
+
+    return sanitizeFruit;
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
-  test('should take two parameters', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
-  });
+    test('should take two parameters', () => {
+        // TODO replace next line with your code
+        expect(sanitizeFruitBasket(fruitBasket, 'lemon')).toHaveLength(4);
+    });
 
-  test('should not modify the original `fruitBasket` array', () => {
-    // Save the original contents of the fruit basket
-    const originalFruitBasketContents = [...fruitBasket];
-    // TODO replace next line with your code
-    expect(false).toBe(true);
-  });
+    test('should not modify the original `fruitBasket` array', () => {
+        // Save the original contents of the fruit basket
+        const originalFruitBasketContents = [...fruitBasket];
+        // TODO replace next line with your code
+        expect(originalFruitBasketContents).toHaveLength(7);
+    });
 
-  test('should return a new array that does not include the unwanted `lemon`', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
-  });
+    test('should return a new array that does not include the unwanted `lemon`', () => {
+        // TODO replace next line with your code
+        const result = sanitizeFruitBasket(fruitBasket, 'lemon');
+        const expected = ['apple', 'grapefruit', 'banana', 'watermelon'];
+        expect(result).toStrictEqual(expected);
+    });
 });
--- 1-JavaScript/Week4/homework/ex4-observable/ex4-observable.js
+++ 1-JavaScript/Week4/homework/ex4-observable/ex4-observable.js
@@ -14,15 +14,21 @@ Complete the `createObservable()` function as follows:
 ------------------------------------------------------------------------------*/
 
 function createObservable() {
-  const subscribers = [];
-  return {
-    subscribe: function (subscriber) {
-      // TODO complete this function
-    },
-    notify: function (message) {
-      // TODO complete this function
-    },
-  };
+    const subscribers = [];
+    return {
+        subscribe: function(subscriber) {
+
+            subscribers.push(subscriber);
+
+        },
+        notify: function(message) {
+            subscribers.forEach(function(subscriber) {
+                subscriber(message);
+            })
+
+
+        },
+    };
 }
 
 // ! Do not change or remove the code below
